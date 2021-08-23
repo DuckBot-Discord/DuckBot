0aba9dd8f31d7a44687ffb79202ab9874db7c703
LeoCx1000 <leonardocavenago05@gmail.com>
2021-08-23 18:50:26 +0000

    edits

--- ozbot/cogs/modmail.py
+++ ozbot/cogs/modmail.py
@@ -1,6 +1,19 @@
 import discord, asyncio, typing, re, yaml
+from discord.errors import HTTPException
 from discord.ext import commands
+from discord.ext.commands.errors import UserNotFound
 from discord.ext.commands.cooldowns import BucketType
+
+async def get_webhook(channel):
+    hookslist = await channel.webhooks()
+    if hookslist:
+        for hook in hookslist:
+            if hook.token:
+                return hook
+            else:
+                continue
+    hook = await channel.create_webhook(name="DuckBot ModMail")
+    return hook
 
 class modmail(commands.Cog):
     """🎫 Dm the bot for help!"""
@@ -15,150 +28,107 @@ class modmail(commands.Cog):
         self.staff_roles = staff_roles
         self.yaml_data = full_yaml
 
-    #--------------- FUNCTIONS ---------------#
-
-    async def perms_error(self, ctx):
-        await ctx.message.add_reaction('🚫')
-        await asyncio.sleep(self.yaml_data['ReactionTimeout'])
-        try:
-            await ctx.message.delete()
+    @commands.Cog.listener('on_message')
+    async def modmail(self, message):
+        if message.guild or message.author == self.bot.user:
             return
-        except: return
 
-    async def error_message(self, ctx, message):
-        embed = discord.Embed(color=ctx.me.color)
-        embed.set_author(name=message, icon_url='https://i.imgur.com/OAmzSGF.png')
-        await ctx.send(embed=embed, delete_after=self.yaml_data['ErrorMessageTimeout'])
-        await asyncio.sleep(self.yaml_data['ErrorMessageTimeout'])
-        try:
-            await ctx.message.delete()
-            return
-        except: return
+        category = self.bot.get_guild(706624339595886683).get_channel(879414245052284958)
+        channel = discord.utils.get(category.channels, name=str(message.author.id))
+        if not channel:
+            channel = await category.create_text_channel(
+                name=str(message.author.id),
+                topic=f"{message.author}'s DMs",
+                position=0,
+                reason="OzBot ModMail"
+        )
+        wh = await get_webhook(channel)
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.guild: return
-        if message.author == self.bot.user: return
-        if message.content.startswith('!'):
-            await message.channel.send('⚠ messages starting with `!` will not be sent ⚠', delete_after=5)
-            return
-        pattern = '^([0-9]{4})'
-        test_string = message.content
-        result = re.match(pattern, test_string)
-
-        if result and len(message.content) == 4:
-            await message.channel.send(f"""
-hey {message.author.mention}, this is NOT the bot to use to link your discord to minecraft
-
-The the bot you're looking for is <@799749818062077962> (nicknamed `@SMP chat` in our discord server)
-
-If the user above says `@invalid-user` and you can't click it to open a direct message you must go to the channel <#799483071069945866> (`#{self.bot.get_channel(799483071069945866).name}`) in our discord server and look for it in the member list to the side
-
-It should look like this: https://i.imgur.com/2py4Csc.png
-""", delete_after=60)
-            return
-        channel = self.bot.get_channel(self.yaml_data['ModMailChannel'])
-        embed = discord.Embed(color=0xD7342A)
-        if message.content:
-            embed.add_field(name=f'<:incomingarrow:848312881070080001> **{message.author}**', value=f'{message.content}')
-        else:
-            embed.add_field(name=f'<:incomingarrow:848312881070080001> **{message.author}**', value=f'_ _')
-        embed.set_footer(text=f'!dm {message.author.id}')
+        files = []
         if message.attachments:
-            file = message.attachments[0]
-            spoiler = file.is_spoiler()
-            if not spoiler and file.url.lower().endswith(('png', 'jpeg', 'jpg', 'gif', 'webp')):
-                embed.set_image(url=file.url)
-            elif spoiler:
-                embed.add_field(name='Attachment', value=f'||[{file.filename}]({file.url})||', inline=False)
-            else:
-                embed.add_field(name='Attachment', value=f'[{file.filename}]({file.url})', inline=False)
-        await channel.send(embed=embed)
-        await message.add_reaction('📬')
-        await asyncio.sleep(2.5)
-        await message.remove_reaction('📬', self.bot.user)
+            for attachment in message.attachments:
+                if attachment.size > 8388600:
+                    await message.author.send('Sent message without attachment! File size greater than 8 MB.')
+                    continue
+                files.append(await attachment.to_file(spoiler=attachment.is_spoiler()))
+
+        try:
+            await wh.send(content=message.content, username=message.author.name, avatar_url=message.author.avatar.url,
+                          files=files)
+        except:
+            return await message.add_reaction('⚠')
+
+    @commands.Cog.listener('on_message')
+    async def modmail_reply(self, message):
+        if any((not message.guild, message.author.bot, message.channel.category_id != 879414245052284958)):
+            return
+
+        channel = message.channel
+
+        try:
+            user = self.bot.get_user(int(channel.name)) or (await self.bot.fetch_user(int(channel.name)))
+        except (HTTPException, UserNotFound):
+            return await channel.send("could not find user.")
+
+        files = []
+        if message.attachments:
+            for attachment in message.attachments:
+                if attachment.size > 8388600:
+                    await message.author.send('Sent message without attachment! File size greater than 8 MB.')
+                    continue
+                files.append(await attachment.to_file(spoiler=attachment.is_spoiler()))
+
+        try:
+            await user.send(content=message.content, files=files)
+        except:
+            return await message.add_reaction('⚠')
 
     @commands.command(aliases=['pm', 'message', 'direct'])
-    async def dm(self, ctx, member: typing.Optional[discord.Member], *, message = None):
+    @commands.has_permissions(manage_messages=True)
+    @commands.guild_only()
+    async def dm(self, ctx, member: discord.Member, *, message = None):
+        if ctx.channel.category_id == 879414245052284958: return
+
         if not any(role in self.staff_roles for role in ctx.author.roles):
-            await self.perms_error(ctx)
-            return
-        if member == None:
-            await ctx.message.add_reaction('⁉')
-            await asyncio.sleep(5)
-            await ctx.message.delete()
-            return
-        channel = self.bot.get_channel(self.yaml_data['ModMailChannel'])
+            raise discord.MissingPermissions('staff_role')
+
+        category = self.bot.get_guild(706624339595886683).get_channel(879414245052284958)
+        channel = discord.utils.get(category.channels, name=str(member.id))
+        if not channel:
+            channel = await category.create_text_channel(
+                name=str(member.id),
+                topic=f"{member}'s DMs",
+                position=0,
+                reason="OzBot ModMail"
+        )
+
+        wh = await get_webhook(channel)
+
+        files = []
+        if ctx.message.attachments:
+            for attachment in ctx.message.attachments:
+                if attachment.size > 8388600:
+                    await ctx.send('Sent message without attachment! File size greater than 8 MB.')
+                    continue
+                files.append(await attachment.to_file(spoiler=attachment.is_spoiler()))
+
         try:
-            await ctx.message.delete()
-        except discord.Forbidden:
+            await member.send(content=message, files=files)
+            try:
+                await ctx.message.delete()
+            except:
+                pass
+        except:
+            return await ctx.message.add_reaction('⚠')
+
+        try:
+            await wh.send(content=message, username=ctx.author.name, avatar_url=ctx.author.avatar.url,
+                          files=files)
+        except:
             pass
-        try:
-            if ctx.message.attachments:
-                file = ctx.message.attachments[0]
-                myfile = await file.to_file()
-                embed = discord.Embed(color=0x47B781)
-                if message:
-                    embed.add_field(name=f'<:outgoingarrow:848312880679354368> **{member.name}#{member.discriminator}**', value=message)
-                    await member.send(message, file=myfile)
-                else:
-                    embed.add_field(name=f'<:outgoingarrow:848312880679354368> **{member.name}#{member.discriminator}**', value='_ _')
-                    await member.send(file=myfile)
-                if ctx.message.attachments:
-                    file = ctx.message.attachments[0]
-                    spoiler = file.is_spoiler()
-                    if not spoiler and file.url.lower().endswith(('png', 'jpeg', 'jpg', 'gif', 'webp')):
-                        embed.set_image(url=file.url)
-                    elif spoiler:
-                        embed.add_field(name='Attachment', value=f'||[{file.filename}]({file.url})||', inline=False)
-                    else:
-                        embed.add_field(name='Attachment', value=f'[{file.filename}]({file.url})', inline=False)
-                embed.set_footer(text=f'!dm {member.id}')
-                await channel.send(embed=embed)
-            else:
-                await member.send(message)
-                embed = discord.Embed(color=0x47B781)
-                embed.add_field(name=f'<:outgoingarrow:848312880679354368> **{member.name}#{member.discriminator}**', value=message)
-                embed.set_footer(text=f'!dm {member.id}')
-                await channel.send(embed=embed)
-        except discord.Forbidden:
-            await ctx.send(f"{member}'s DMs are closed.")
 
-    @dm.error
-    async def dm_error(self, ctx, error):
-        if isinstance(error, commands.CheckFailure):
-            await ctx.message.add_reaction('🚫')
-            await asyncio.sleep (5)
-            await ctx.message.delete()
 
-    @commands.command(aliases=['spm', 'smessage', 'sdirect'])
-    async def sdm(self, ctx, member: typing.Optional[discord.Member], *, message = ""):
-        if not any(role in self.staff_roles for role in ctx.author.roles):
-            await self.perms_error(ctx)
-            return
-        if member == None:
-            await ctx.message.add_reaction('⁉')
-            await asyncio.sleep(5)
-            await ctx.message.delete()
-            return
-        await ctx.message.delete()
-        try:
-            if ctx.message.attachments:
-                file = ctx.message.attachments[0]
-                myfile = await file.to_file()
-                await member.send(message, file=myfile)
-                embed = discord.Embed(color=0x47B781)
-            else:
-                await member.send(message)
-        except discord.Forbidden:
-            await ctx.send(f"{member}'s DMs are closed.")
 
-    @sdm.error
-    async def dm_error(self, ctx, error):
-        if isinstance(error, commands.CheckFailure):
-            await ctx.message.add_reaction('🚫')
-            await asyncio.sleep (5)
-            await ctx.message.delete()
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
