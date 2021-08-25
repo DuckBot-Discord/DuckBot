c7ca927d0eb4ae582a9cc88849a00d15fc11eda1
LeoCx1000 <leonardocavenago05@gmail.com>
2021-08-25 15:18:10 -0500

    adding new error handlers

--- TargetBot/cogs/handler.py
+++ TargetBot/cogs/handler.py
@@ -1,94 +1,173 @@
+import io
+import logging
+import traceback
 import  discord, asyncio
 from discord.ext import commands
 from discord.ext.commands import BucketType
 
-class handler(commands.Cog):
-    """🆘 Handle them errors 👀"""
+
+
+class Handler(commands.Cog, name='Handler'):
+    """
+    🆘 Handle them errors 👀
+    """
+
     def __init__(self, bot):
         self.bot = bot
+        self.error_channel = 880181130408636456
 
-    async def perms_error(self, ctx):
-        await ctx.message.add_reaction('🚫')
-        try: await ctx.message.delete(delay=5)
-        except: pass
-
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
+    @commands.Cog.listener('on_command_error')
+    async def error_handler(self, ctx: commands.Context, error):
         error = getattr(error, "original", error)
+        ignored = (
+            commands.CommandNotFound,
+        )
+        if isinstance(error, ignored):
+            return
 
         if isinstance(error, discord.ext.commands.CheckAnyFailure):
             for e in error.errors:
-                if error != commands.NotOwner:
+                if not isinstance(error, commands.NotOwner):
                     error = e
                     break
 
+        if isinstance(error, discord.ext.commands.BadUnionArgument):
+            if error.errors:
+                error = error.errors[0]
+
         embed = discord.Embed(color=0xD7342A)
-        embed.set_author(name = 'Missing permissions!', icon_url='https://i.imgur.com/OAmzSGF.png')
+        embed.set_author(name='Missing permissions!',
+                         icon_url='https://i.imgur.com/OAmzSGF.png')
 
         if isinstance(error, commands.NotOwner):
-            await ctx.send(f"you must own `{ctx.me.display_name}` to use `{ctx.command}`")
-            return
+            return await ctx.send(f"you must own `{ctx.me.display_name}` to use `{ctx.command}`")
+
+        if isinstance(error, commands.TooManyArguments):
+            return await ctx.send(f"Too many arguments passed to the command!")
 
         if isinstance(error, discord.ext.commands.MissingPermissions):
-            text=f"You're missing the following permissions: \n**{', '.join(error.missing_perms)}**"
+            text = f"You're missing the following permissions: \n**{', '.join(error.missing_permissions)}**"
+            embed.description = text
             try:
-                embed.description=text
-                await ctx.send(embed=embed)
-            except:
-                try: await ctx.send(text)
-                except: pass
-            return
+                return await ctx.send(embed=embed)
+            except discord.Forbidden:
+                try:
+                    return await ctx.send(text)
+                except discord.Forbidden:
+                    pass
+                finally:
+                    return
 
         if isinstance(error, discord.ext.commands.BotMissingPermissions):
-            text=f"I'm missing the following permissions: \n**{', '.join(error.missing_perms)}**"
+            text = f"I'm missing the following permissions: \n**{', '.join(error.missing_permissions)}**"
             try:
-                embed.description=text
+                embed.description = text
                 await ctx.send(embed=embed)
-            except:
-                try: await ctx.send(text)
-                except: pass
-            return
+            except discord.Forbidden:
+                await ctx.send(text)
+            finally:
+                return
 
         elif isinstance(error, discord.ext.commands.MissingRequiredArgument):
-            missing=f"{str(error.param).split(':')[0]}"
-            command = f"{ctx.prefix}{ctx.command} {ctx.command.signature}"
-            separator = (' ' * (len(command.split(missing)[0])-1))
-            indicator = ('^' * (len(missing)+2))
-            print(f"`{separator}`  `{indicator}`")
-            print(error.param)
-            print()
-            await ctx.send(f"""```
-{command}
-{separator}{indicator}
-{missing} is a required argument that is missing.
-```""")
+            missing = f"{str(error.param).split(':')[0]}"
+            command = f"{ctx.clean_prefix}{ctx.command} {ctx.command.signature}"
+            separator = (' ' * (len(command.split(missing)[0]) - 1))
+            indicator = ('^' * (len(missing) + 2))
+
+            logging.info(f"`{separator}`  `{indicator}`")
+            logging.info(error.param)
+
+            return await ctx.send(
+                f"```{command}\n{separator}{indicator}\n{missing} is a required argument that is missing.\n```")
 
         elif isinstance(error, commands.errors.PartialEmojiConversionFailure):
-            await ctx.send(f"`{error.argument}` is not a valid Custom Emoji")
+            return await ctx.send(f"`{error.argument}` is not a valid Custom Emoji")
 
         elif isinstance(error, commands.errors.CommandOnCooldown):
-            embed = discord.Embed(color=0xD7342A, description = f'Please try again in {round(error.retry_after, 2)} seconds')
-            embed.set_author(name = 'Command is on cooldown!', icon_url='https://i.imgur.com/izRBtg9.png')
+            embed = discord.Embed(color=0xD7342A,
+                                  description=f'Please try again in {round(error.retry_after, 2)} seconds')
+            embed.set_author(name='Command is on cooldown!',
+                             icon_url='https://i.imgur.com/izRBtg9.png')
 
-            if error.cooldown.type == BucketType.default: per = ""
-            if error.cooldown.type == BucketType.user: per = "per user"
-            if error.cooldown.type == BucketType.guild: per = "per server"
-            if error.cooldown.type == BucketType.channel: per = "per channel"
-            if error.cooldown.type == BucketType.member: per = "per member"
-            if error.cooldown.type == BucketType.category: per = "per category"
-            if error.cooldown.type == BucketType.role: per = "per role"
+            if error.type == BucketType.default:
+                per = ""
+            elif error.type == BucketType.user:
+                per = "per user"
+            elif error.type == BucketType.guild:
+                per = "per server"
+            elif error.type == BucketType.channel:
+                per = "per channel"
+            elif error.type == BucketType.member:
+                per = "per member"
+            elif error.type == BucketType.category:
+                per = "per category"
+            elif error.type == BucketType.role:
+                per = "per role"
+            else:
+                per = ""
 
-            embed.set_footer(text=f"cooldown: {error.cooldown.rate} per {error.cooldown.per}s {per}")
+            embed.set_footer(
+                text=f"cooldown: {error.cooldown.rate} per {error.cooldown.per}s {per}")
             return await ctx.send(embed=embed)
 
-        elif isinstance(error, discord.ext.commands.errors.CommandNotFound):
-            pass
+        elif isinstance(error, commands.errors.MemberNotFound):
+            return await ctx.send(f"I couldn't find `{error.argument}` in this server")
 
+        elif isinstance(error, commands.errors.UserNotFound):
+            return await ctx.send(
+                f"I've searched far and wide, but `{error.argument}` doesn't seem to be a member discord user...")
+
+        elif isinstance(error, commands.BadArgument):
+            return await ctx.send(error or "Bad argument given!")
+
+        elif isinstance(error, discord.HTTPException):
+            await ctx.send("Oh no! An unexpected HTTP error occurred while handling this command! 😔"
+                           "\nI've notified the developers about it. in the meantime, maybe try again?")
+
+        elif isinstance(error, discord.Forbidden):
+            await ctx.send("Oh no! It seems like I don't have permissions to perform that action!"
+                           "\nThis may be due to me missing permissions in a specific channel, server"
+                           "permissions, or an issue with role hierarchy. Try adjusting my permissions"
+                           "for this server. \n(Note that I can't edit the server owner)")
+
+        error_channel = self.bot.get_channel(self.error_channel)
+
+        traceback_string = "".join(traceback.format_exception(
+            etype=None, value=error, tb=error.__traceback__))
+
+        await self.bot.wait_until_ready()
+
+        if ctx.guild:
+            command_data = f"command: {ctx.message.content[0:1700]}" \
+                           f"\nguild_id: {ctx.guild.id}" \
+                           f"\nowner_id: {ctx.guild.owner.id}" \
+                           f"\nbot admin: {await ctx.default_tick(ctx.me.guild_permissions.administrator)} " \
+                           f"- role pos: {ctx.me.top_role.position}"
         else:
-            await self.bot.wait_until_ready()
-            await self.bot.get_channel(847943387083440128).send(f"""```{ctx.command} command raised an error:
-    {error}```""")
-            raise error
+            command_data = f"command: {ctx.message.content[0:1700]}" \
+                           f"\nCommand executed in DMs"
+
+        to_send = f"```yaml\n{command_data}``````py\n{ctx.command} " \
+                  f"command raised an error:\n{traceback_string}\n```"
+        if len(to_send) < 2000:
+            try:
+                sent_error = await error_channel.send(to_send)
+
+            except (discord.Forbidden, discord.HTTPException):
+                sent_error = await error_channel.send(f"```yaml\n{command_data}``````py Command: {ctx.command}"
+                                                      f"Raised the following error:\n```",
+                                                      file=discord.File(io.StringIO(traceback_string),
+                                                                        filename='traceback.py'))
+        else:
+            sent_error = await error_channel.send(f"```yaml\n{command_data}``````py Command: {ctx.command}"
+                                                  f"Raised the following error:\n```",
+                                                  file=discord.File(io.StringIO(traceback_string),
+                                                                    filename='traceback.py'))
+        try:
+            await sent_error.add_reaction('🗑')
+        except (discord.HTTPException, discord.Forbidden):
+            pass
+        raise error
 
 def setup(bot):
     bot.add_cog(handler(bot))
