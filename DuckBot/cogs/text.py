b25f04368d68b7d24ecccbce26d37399d088ad64
LeoCx1000 <leonardocavenago05@gmail.com>
2021-08-25 02:49:26 -0500

    oops typo

--- DuckBot/cogs/text.py
+++ DuckBot/cogs/text.py
@@ -68,7 +68,7 @@ class General(commands.Cog):
 
     @commands.command(
         aliases=['a', 'an', 'announce'],
-        usage="<message or reply>")
+        usage="<channel> <message or reply>")
     @commands.check_any(commands.has_permissions(manage_messages=True), commands.is_owner())
     @commands.check_any(commands.bot_has_permissions(send_messages=True, manage_messages=True), commands.is_owner())
     async def echo(self, ctx: commands.Context, channel: discord.TextChannel, *, message_or_reply: str = None) -> discord.Message:
