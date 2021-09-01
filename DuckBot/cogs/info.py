7b719722372441e5132e9bb151553827188ac59c
LeoCx1000 <leonardocavenago05@gmail.com>
2021-09-01 10:03:37 -0500

    fixed time

--- DuckBot/cogs/info.py
+++ DuckBot/cogs/info.py
@@ -236,7 +236,7 @@ class MyHelp(commands.HelpCommand):
 
         embed.add_field(name=f"Available categories [{len(all_cogs)}]", value=f"```fix\n{nl.join(all_cogs)}```")
 
-        embed.add_field(name="📰 Latest News - <t:1630462700:d> (<t:1630462700:R>)", value=f"""
+        embed.add_field(name="📰 Latest News - <t:1630508600:d> (<t:1630508600:R>)", value=f"""
 _ _
 > 🎓 **Added proper credits to the help command**
 
