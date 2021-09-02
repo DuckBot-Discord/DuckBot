5326b53ac47e333106e1dc4e8a901888252efa43
LeoCx1000 <leonardocavenago05@gmail.com>
2021-09-02 01:11:57 -0500

    custom send maybe?

--- DuckBot/main.py
+++ DuckBot/main.py
@@ -57,7 +57,7 @@ class CustomContext(commands.Context):
             return f"{emoji} {text}"
         return emoji
 
-    async def send(self, content: str = None, embed: discord.Embed = None,
+    async def send(self, content: str = None, embed: discord.Embed = None, reply: bool = True,
                    footer: bool = True, mention_author: bool = False, **kwargs):
         if embed and footer is True:
             if not embed.footer:
@@ -65,7 +65,8 @@ class CustomContext(commands.Context):
                                  icon_url=self.author.display_avatar.url)
                 embed.timestamp = discord.utils.utcnow()
 
-        return await super().send(content=content, embed=embed, mention_author=mention_author, **kwargs)
+        return await super().send(content=content, embed=embed, mention_author=mention_author, **kwargs) if not reply \
+            else self.reply(content=content, embed=embed, mention_author=mention_author, **kwargs)
 
 
 class DuckBot(commands.Bot):
