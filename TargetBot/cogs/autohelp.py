af74ce2f351e51c9e5aa814224e15f7303888b93
LeoCx1000 <leonardocavenago05@gmail.com>
2021-09-26 16:48:24 -0500

    emoji command changes

--- TargetBot/cogs/autohelp.py
+++ TargetBot/cogs/autohelp.py
@@ -1,6 +1,11 @@
-import json, random, discord, aiohttp, typing, asyncio, yaml, re, urllib, yarl
-from random import randint
+import aiohttp
+import discord
+import re
+import urllib
+import yaml
+import yarl
 from discord.ext import commands
+
 
 class automod(commands.Cog):
 
@@ -29,30 +34,36 @@ class automod(commands.Cog):
         for link in results:
             url = urllib.parse.quote(link, safe='')
             async with aiohttp.ClientSession() as cs:
-                async with cs.get(yarl.URL(f"https://ipqualityscore.com/api/json/url/{self.token}/{url}",encoded=True)) as r:
+                async with cs.get(
+                        yarl.URL(f"https://ipqualityscore.com/api/json/url/{self.token}/{url}", encoded=True)) as r:
                     text = await r.json()
                     await ctx.send(text)
 
         await ctx.message.remove_reaction('🔃', self.bot.user)
 
-
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.author.bot: return
-        if not message.guild: return
-        if message.guild.id != 717140270789033984: return
-        if message.channel.permissions_for(message.guild.default_role).send_messages:
-            if any(item in self.exempt_roles for item in message.author.roles): return
+        if message.author.bot:
+            return
+        if not message.guild:
+            return
+        if message.guild.id != 717140270789033984:
+            return
+        if message.channel.permissions_for(message.guild.default_role).send_messages and not \
+                message.channel.permissions_for(message.author).manage_messages:
+            if any(item in self.exempt_roles for item in message.author.roles):
+                return
             if "download" in f"{message.content.lower()}":
                 if self.bot.get_guild(717140270789033984).get_role(717144906350592061) in message.author.roles:
-                    embed=discord.Embed(title="", description="""I see you're asking about downloads. To access the download channel you need to have a role `Steeler` or higher, but you seem to have the <@&717144906350592061> role.
+                    embed = discord.Embed(title="", description="""I see you're asking about downloads. To access the download channel you need to have a role `Steeler` or higher, but you seem to have the <@&717144906350592061> role.
 You can get a `Steeler` or higher subscription here: [patreon.com/stylized](https://www.patreon.com/Stylized).
 _if you already have one, unlink and relink your patreon_""", color=message.guild.me.color)
                 else:
-                    embed=discord.Embed(title="", description="""I see you're asking about downloads. To access the download channel you need to have a role `Steeler` or higher, but you don't seem to have any roles.
+                    embed = discord.Embed(title="", description="""I see you're asking about downloads. To access the download channel you need to have a role `Steeler` or higher, but you don't seem to have any roles.
 If you already purchased a `Steeler` or higher subscription, link your Patreon to Discord. [[more info]](https://support.patreon.com/hc/en-us/articles/212052266-Get-my-Discord-role#:~:text=Step%201%3A%20Log%20in%20to,role%20tied%20to%20your%20Tier!)
 If your account is already linked, unlink and relink it. [[more info]](https://support.patreon.com/hc/en-us/articles/212052266-Get-my-Discord-role#:~:text=Step%201%3A%20Log%20in%20to,role%20tied%20to%20your%20Tier!) about how to get your role.
-If you don't already have a `Steeler` or higher subscription, you can get one at [patreon.com/stylized](https://www.patreon.com/Stylized).""", color=message.guild.me.color)
+If you don't already have a `Steeler` or higher subscription, you can get one at [patreon.com/stylized](https://www.patreon.com/Stylized).""",
+                                          color=message.guild.me.color)
 
                 embed.set_author(name="Automatic support", icon_url="https://i.imgur.com/GTttbJW.png")
                 await message.reply(embed=embed)
