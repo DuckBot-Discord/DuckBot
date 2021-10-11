5b719f5e072d2b8bcd6ea090179ff5f72c4b4e00
Leonardo Cavenago <leonardocavenago05@gmail.com>
2021-10-10 21:15:29 -0500

    fixed muterole create command

--- DuckBot/cogs/events.py
+++ DuckBot/cogs/events.py
@@ -414,7 +414,21 @@ class Handler(commands.Cog, name='Handler'):
         await message.add_reaction(constants.UPVOTE)
         await message.add_reaction(constants.DOWNVOTE)
 
-    @commands.command()
+    @commands.Cog.listener()
     async def on_guild_remove(self, guild: discord.Guild):
-        await self.bot.db.execute('DELETE FROM prefixes WHERE guild_id = $1;'
-                                  '\nDELETE FROM temporary_mutes WHERE guild_id = $1;', guild.id)
+        await self.bot.db.execute('DELETE FROM prefixes WHERE guild_id = $1', guild.id)
+        await self.bot.db.execute('DELETE FROM temporary_mutes WHERE guild_id = $1', guild.id)
+        for channel in guild.text_channels:
+            await self.bot.db.execute('DELETE FROM suggestions WHERE channel_id = $1', channel.id)
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild: discord.Guild):
+        await self.bot.db.execute('DELETE FROM prefixes WHERE guild_id = $1', guild.id)
+        await self.bot.db.execute('DELETE FROM temporary_mutes WHERE guild_id = $1', guild.id)
+        for channel in guild.text_channels:
+            await self.bot.db.execute('DELETE FROM suggestions WHERE channel_id = $1', channel.id)
+
+    @commands.Cog.listener()
+    async def on_guild_channel_delete(self, channel: discord.abc.GuildChannel):
+        if channel.id in self.bot.suggestion_channels:
+            await self.bot.db.execute('DELETE FROM suggestions WHERE channel_id = $1', channel.id)
