21de7bd04f465d8e3bcf9d8d3369693ef652d675
Leonardo Cavenago <leonardocavenago05@gmail.com>
2021-11-16 19:50:33 -0500

    maybe update blackout mode?

--- ospbot/main.py
+++ ospbot/main.py
@@ -1,3 +1,7 @@
+import io
+import logging
+import traceback
+
 import asyncpg
 import discord
 import os
@@ -10,6 +14,10 @@ from dotenv import load_dotenv
 with open(r'files/config.yaml') as file:
     full_yaml = yaml.full_load(file)
 yaml_data = full_yaml
+
+
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='[%(asctime)-15s] %(message)s')
 
 
 async def create_db_pool():
@@ -66,15 +74,15 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    prefixes = ('.')
-    if bot.maintenance == True:
+    prefixes = ('.',)
+    if bot.maintenance is True:
         if message.author.id in bot.owner_ids:
             await bot.process_commands(message)
             return
         if message.content.startswith(prefixes):
             return
         return
-    if not message.content.startswith(prefixes) and message.author.id in bot.owner_ids and bot.noprefix == True:
+    if not message.content.startswith(prefixes) and message.author.id in bot.owner_ids and bot.noprefix is True:
         edited_message = message
         edited_message.content = f".{message.content}"
         await bot.process_commands(edited_message)
@@ -96,6 +104,29 @@ for filename in os.listdir("./cogs"):
             print(f"\033[91mAn error occurred while loading '{filename}'""")
             print('\033[0m')
 print('\033[0m')
+
+
+@bot.event
+async def on_error(self, event_method: str, *args, **kwargs) -> None:
+    traceback_string = traceback.format_exc()
+    for line in traceback_string.split('\n'):
+        logging.info(line)
+    await self.wait_until_ready()
+    error_channel = self.get_channel(880181130408636456)
+    to_send = f"```yaml\nAn error occurred in an {event_method} event``````py" \
+              f"\n{traceback_string}\n```"
+    if len(to_send) < 2000:
+        try:
+            await error_channel.send(to_send)
+
+        except (discord.Forbidden, discord.HTTPException):
+
+            await error_channel.send(f"```yaml\nAn error occurred in an {event_method} event``````py",
+                                     file=discord.File(io.StringIO(traceback_string), filename='traceback.py'))
+    else:
+        await error_channel.send(f"```yaml\nAn error occurred in an {event_method} event``````py",
+                                 file=discord.File(io.StringIO(traceback_string), filename='traceback.py'))
+
 
 bot.loop.run_until_complete(create_db_pool())
 bot.run(TOKEN, reconnect=True)
