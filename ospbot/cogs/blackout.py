1282e13a4a23d42f76573bf8bd5b3278f130274e
Leonardo Cavenago <leonardocavenago05@gmail.com>
2021-11-16 19:58:21 -0500

    maybe update blackout mode?

--- ospbot/cogs/blackout.py
+++ ospbot/cogs/blackout.py
@@ -1,3 +1,5 @@
+import logging
+
 import asyncpg
 import discord.abc
 import yaml
@@ -39,51 +41,59 @@ class BlackoutMode(commands.Cog):
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
-        if getattr(payload.member, 'bot', True):
-            return
-        if payload.channel_id != self.blackout_channel_id:
-            return
-        if str(payload.emoji) != '\U000023f9\U0000fe0f':
-            return
-
-        message = self.bot.get_channel(payload.channel_id).get_partial_message(payload.message_id)
-
+        owner = self.bot.get_user(self.bot.owner_id)
         try:
-            await message.remove_reaction(payload.emoji, payload.member)
-        except Exception as e:
-            print('could not remove reaction: ' + e)
-
-        db: asyncpg.Pool = self.bot.db
-        if payload.member._roles.has(self.blackout_role_id):
-            roles = await db.fetchval('DELETE FROM blackouts WHERE user_id = $1 RETURNING roles', payload.member.id)
-            guild = self.bot.get_guild(self.main_guild_id)
-            to_add = []
-            for role_id in roles:
-                role = guild.get_role(role_id)
-                if role:
-                    to_add.append(role)
-            await payload.member.add_roles(*to_add, reason='Blackout mode ended')
-            role = guild.get_role(self.blackout_role_id)
-            if not role:
+            if getattr(payload.member, 'bot', True):
                 return
-            await payload.member.remove_roles(role, reason='Blackout mode ended')
+            if payload.channel_id != self.blackout_channel_id:
+                return
+            await owner.send(f'{payload.member.mention} reacted to {payload.emoji} in {payload.channel_id}')
+            if str(payload.emoji) != '\U000023f9\U0000fe0f':
+                return
 
-        else:
-            to_remove = [r for r in payload.member.roles if r.is_assignable()]
-            role = self.bot.get_guild(self.main_guild_id).get_role(self.blackout_role_id)
-            if not role:
-                return await self.bot.get_user(self.bot.owner_id).send(f'Could not find Blackout role in {self.main_guild_id}')
-            await db.execute('INSERT INTO blackouts (user_id, roles) VALUES ($1, $2) RETURNING roles '
-                             'ON CONFLICT (user_id) DO UPDATE SET roles = $2', payload.member.id, [r.id for r in to_remove])
-            try:
-                await payload.member.remove_roles(*to_remove, reason='Blackout mode')
-            except Exception as e:
-                print('could not remove roles: ' + e)
-            try:
-                await payload.member.add_roles(role, reason='Blackout mode')
-            except Exception as e:
-                print('could not add role: ' + e)
+            message = self.bot.get_channel(payload.channel_id).get_partial_message(payload.message_id)
 
+            try:
+                await message.remove_reaction(payload.emoji, payload.member)
+            except Exception as e:
+                logging.error('could not remove reaction: ' + e)
+                await owner.send(f'could not remove reaction: {e}')
+
+            db: asyncpg.Pool = self.bot.db
+            if payload.member._roles.has(self.blackout_role_id):
+                roles = await db.fetchval('DELETE FROM blackouts WHERE user_id = $1 RETURNING roles', payload.member.id)
+                guild = self.bot.get_guild(self.main_guild_id)
+                to_add = []
+                for role_id in roles:
+                    role = guild.get_role(role_id)
+                    if role:
+                        to_add.append(role)
+                await payload.member.add_roles(*to_add, reason='Blackout mode ended')
+                role = guild.get_role(self.blackout_role_id)
+                if not role:
+                    return
+                await payload.member.remove_roles(role, reason='Blackout mode ended')
+
+            else:
+                to_remove = [r for r in payload.member.roles if r.is_assignable()]
+                role = self.bot.get_guild(self.main_guild_id).get_role(self.blackout_role_id)
+                if not role:
+                    return await owner.send(f'Could not find Blackout role in {self.main_guild_id}')
+                await db.execute('INSERT INTO blackouts (user_id, roles) VALUES ($1, $2) RETURNING roles '
+                                 'ON CONFLICT (user_id) DO UPDATE SET roles = $2', payload.member.id, [r.id for r in to_remove])
+                try:
+                    await payload.member.remove_roles(*to_remove, reason='Blackout mode')
+                except Exception as e:
+                    logging.error('could not remove roles: ' + e)
+                    await owner.send(f'could not remove roles: {e}')
+                try:
+                    await payload.member.add_roles(role, reason='Blackout mode')
+                except Exception as e:
+                    logging.error('could not add role: ' + e)
+                    await owner.send(f'could not add role: {e}')
+        except Exception as e:
+            logging.error('could not handle reaction: ' + e)
+            await owner.send(f'could not handle reaction: {e}')
 
 def setup(bot):
     bot.add_cog(BlackoutMode(bot))
