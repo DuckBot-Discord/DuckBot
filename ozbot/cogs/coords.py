0fc90a55c898306d7d217658154ca81cc109dacd
Leonardo Cavenago <leonardocavenago05@gmail.com>
2021-11-20 02:28:06 -0500

    oz bot stuff

--- ozbot/cogs/coords.py
+++ ozbot/cogs/coords.py
@@ -16,23 +16,23 @@ class Coords(commands.Cog):
         self.bot: Ozbot = bot
 
     @commands.command(name='save', aliases=['save-coords'], brief='Saves your coordinates to the database.', slash_command=True, slash_command_guilds=[706624339595886683])  # Dont ask for fork they all shit!
-    async def save_coords(self, ctx: commands.Context, x: int, y: int, z: int, *, description: discord.utils.remove_markdown):
+    async def save_coords(self, ctx: commands.Context, x: int, z: int, *, description: discord.utils.remove_markdown):
         """ Saves a coordinate to the public database """
         try:
-            await self.bot.db.execute("INSERT INTO coords (author, x, y, z, description) VALUES ($1, $2, $3, $4, $5)",
-                                      ctx.author.id, x, y, z, description)
+            await self.bot.db.execute("INSERT INTO coords (author, x, z, description) VALUES ($1, $2, $3, $4)",
+                                      ctx.author.id, x, z, description)
         except asyncpg.UniqueViolationError:
             return await ctx.send("Someone has already saved that coordinate to the global spreadsheet!", ephemeral=True)
-        await ctx.send(f"Coordinate `{x}X {y}Y {z}Z` saved with annotation: `{discord.utils.remove_markdown(description)}`")
+        await ctx.send(f"Coordinate `{x}X {z}Z` saved with annotation: `{discord.utils.remove_markdown(description)}`")
 
     @commands.command(name='list', aliases=['list-coords', 'coords'], brief='Lists all coordinates saved by you.', slash_command=True, slash_command_guilds=[706624339595886683])  # Dont ask for fork they all shit!
     async def list_coords(self, ctx: commands.Context):
         """ Lists all coordinates saved to the database """
-        coords = await self.bot.db.fetch("SELECT author, x, y, z, description FROM coords")
+        coords = await self.bot.db.fetch("SELECT author, x, z, description FROM coords")
         if not coords:
             return await ctx.send("There are no coordinates saved! Do `/save` to save one.")
-        table = [(self.bot.get_user(author) or author, str(x), str(y), str(z), brief) for author, x, y, z, brief in coords]
-        table = tabulate.tabulate(table, headers=["Author", "X", "Y", "Z", "Description"], tablefmt="presto")
+        table = [(self.bot.get_user(author) or author, str(x), str(z), brief) for author, x, z, brief in coords]
+        table = tabulate.tabulate(table, headers=["Author", "X", "Z", "Description"], tablefmt="presto")
         lines = table.split("\n")
         lines, headers = lines[2:], '\n'.join(lines[0:2])
         header = f"All global coords. do /save to save one".center(len(lines[0]))
@@ -56,12 +56,6 @@ class Coords(commands.Cog):
 #         {
 #             "name": "x",
 #             "description": "X Coordinate",
-#             "type": 4,
-#             "required": True
-#         },
-#         {
-#             "name": "y",
-#             "description": "Y coodrinate",
 #             "type": 4,
 #             "required": True
 #         },
