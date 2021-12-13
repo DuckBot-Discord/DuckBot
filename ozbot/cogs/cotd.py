74d6cf576bdd6a340a0c8b5e236b4e0df64b7a53
Leonardo Cavenago <leonardocavenago05@gmail.com>
2021-12-13 01:41:56 -0500

    maybe redo cotd

--- ozbot/cogs/cotd.py
+++ ozbot/cogs/cotd.py
@@ -3,12 +3,48 @@ import datetime
 import discord
 import typing
 import urllib
+import colorsys
 
+import random
 from discord.ext import commands, tasks
 
 q = "'"
 t = ' now'
 n = ''
+
+st_x = 0
+
+
+def naturalize(number: int) -> int:
+    if number > 360:
+        return number - 360
+    elif number < 0:
+        return number + 360
+    else:
+        return number
+
+
+def _get_range(x: int, aperture) -> list:
+    x = int(x)
+    r = list(range(x - aperture, x + aperture))
+    r2 = list(map(naturalize, r))
+    return r2
+
+
+def _get_degree(initial, aperture: int = 20):
+    mrange = _get_range(initial * 360, aperture=aperture)
+    degrees = random.uniform(0, 1) * 360
+    while int(degrees) in mrange:
+        degrees = random.uniform(0, 1) * 360
+    return degrees / 360
+
+
+def random_color(previous_color: discord.Color = None):
+    if previous_color is None:
+        previous_color = discord.Color.random()
+
+    prev_h = colorsys.rgb_to_hsv(previous_color.r / 255, previous_color.g / 255, previous_color.b / 255)
+    return discord.Color.from_hsv(_get_degree(prev_h[0], aperture=20), random.uniform(0.5, 1), random.uniform(0.5, 1))
 
 
 def get_complementary(color):
@@ -20,7 +56,11 @@ def get_complementary(color):
 
 
 async def do_cotd(ctx: typing.Union[commands.Context, commands.Bot], color: discord.Color = None, manual: bool = False):
-    color = color or discord.Color.random()
+    if color is None:
+        previous = await getattr(ctx, 'bot', ctx).db.fetchval('SELECT color_int FROM cotd ORDER BY added_at LIMIT 1')
+        previous = discord.Color(int(previous)) if previous else None
+        color = random_color(previous_color=previous)
+
     guild = (ctx.bot if isinstance(ctx, commands.Context) else ctx).get_guild(706624339595886683)
 
     await guild.get_role(800407956323434556).edit(colour=color)
@@ -29,7 +69,7 @@ async def do_cotd(ctx: typing.Union[commands.Context, commands.Bot], color: disc
     embed = discord.Embed(color=color)
     embed.set_author(icon_url='https://imgur.com/izRBtg9', name='Color of the Day')
     embed.set_image(url=f'https://fakeimg.pl/1200x500/{str(color)[1:]}/{get_complementary(str(color))[1:]}/'
-                    f'?text={urllib.parse.quote(f"Today{q}s color is{t if manual is True else n} {color}")}')
+                        f'?text={urllib.parse.quote(f"Today{q}s color is{t if manual is True else n} {color}")}')
     if manual is True:
         embed.set_footer(text=f"Requested by {ctx.author}",
                          icon_url=ctx.author.display_avatar.url)
@@ -38,11 +78,13 @@ async def do_cotd(ctx: typing.Union[commands.Context, commands.Bot], color: disc
         await ctx.send(embed=embed, delete_after=1)
 
     await log_channel.send(embed=embed)
+    await getattr(ctx, 'bot', ctx).db.execute('INSERT INTO cotd(color_int, added_at) VALUES ($1, $2)', color.value, discord.utils.utcnow())
     return color
 
 
 class daily_color(commands.Cog):
     """🎨 A role that changes color every day."""
+
     def __init__(self, bot):
         self.bot = bot
 
@@ -83,7 +125,7 @@ class daily_color(commands.Cog):
             date = members.joined_at
             now = discord.utils.utcnow()
             diff = now - date
-            hours = diff.total_seconds() / 60 /60
+            hours = diff.total_seconds() / 60 / 60
             if hours >= 336:
                 await members.remove_roles(role)
             await asyncio.sleep(5)
