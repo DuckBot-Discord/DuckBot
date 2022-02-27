190b8cd847c1ed5f37291770e67de3006bdd5d3f
NextChai <75498301+NextChai@users.noreply.github.com>
2022-02-27 16:37:58 -0500

    Add ctx.tick

--- cogs/utils/context.py
+++ cogs/utils/context.py
@@ -1,12 +1,29 @@
 from __future__ import annotations
 
-from typing import Tuple
+from typing import Callable, Tuple, Optional
 
 from discord.ext import commands
 
 __all__: Tuple[str, ...] = (
     'DuckContext',
+    'tick',
 )
 
+def tick(opt: Optional[bool], label: Optional[str] = None) -> str:
+    lookup = {
+        True: '✅',
+        False: '❌',
+        None: '❔',
+    }
+    emoji = lookup.get(opt, '❌')
+    if label is not None:
+        return f'{emoji}: {label}'
+    
+    return emoji
+
+
 class DuckContext(commands.Context):
-    pass
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.tick: Callable[[Optional[bool], Optional[str]], str] = tick
+    
