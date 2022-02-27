5bb420e4a6bb7dfbc000305b9f91d9b889aa229b
NextChai <75498301+NextChai@users.noreply.github.com>
2022-02-27 13:23:31 -0500

    Start from scratch

--- cogs/fun/_base.py
+++
@@ -1,9 +0,0 @@
-import discord
-from discord.ext import commands
-
-from DuckBot.__main__ import DuckBot
-
-
-class FunBase(commands.Cog):
-    def __init__(self, bot: DuckBot):
-        self.bot = bot
--- cogs/utility/_base.py
+++
@@ -1,7 +0,0 @@
-from discord.ext import commands
-from ...__main__ import DuckBot
-
-
-class UtilityBase(commands.Cog):
-    def __init__(self, bot: DuckBot):
-        self.bot = bot
