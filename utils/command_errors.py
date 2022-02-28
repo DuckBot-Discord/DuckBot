4c6d45a6c07186ba1813d2419fdb2576c04e1ea3
Leo <leonardocavenago05@gmail.com>
2022-02-28 19:00:03 +0100

    added a command error event that's reloadable

---
+++ utils/command_errors.py
@@ -0,0 +1,41 @@
+from discord.ext import commands
+
+from bot import DuckBot
+from utils.context import DuckContext
+
+
+async def on_command_error(ctx: DuckContext, error: commands.CommandError) -> None:
+    """|coro|
+
+    A handler called when an error is raised while invoking a command.
+
+    Parameters
+    ----------
+    ctx: :class:`DuckContext`
+        The context for the command.
+    error: :class:`commands.CommandError`
+        The error that was raised.
+    """
+    ignored = (
+        commands.CommandNotFound,
+        commands.CheckFailure
+    )
+    if isinstance(error, ignored):
+        return
+    elif isinstance(error, commands.UserInputError):
+        await ctx.send(error)
+    elif isinstance(error, commands.CommandInvokeError):
+        return await on_command_error(ctx, error.original)  # type: ignore
+    else:
+        await ctx.bot.exceptions.add_error(error=error, ctx=ctx)
+
+
+def setup(bot: DuckBot):
+    """adds the event to the bot
+
+    Parameters
+    ----------
+    bot: :class:`DuckBot`
+        The bot to add the event to.
+    """
+    bot.add_listener(on_command_error)
