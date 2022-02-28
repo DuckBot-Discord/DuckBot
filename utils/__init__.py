4e67f38792d917f0f28cd36b930da29fe0632ff9
NextChai <75498301+NextChai@users.noreply.github.com>
2022-02-27 23:30:19 -0500

    Add the "add_logging" func to utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,11 +1,63 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Tuple
+import asyncio
+import time as time_lib
+from typing import (
+    TYPE_CHECKING,
+    Optional, 
+    Tuple,
+    TypeVar,
+    ParamSpec,
+    Callable,
+    Awaitable,
+    Union,
+)
 
 from discord.ext import commands
 
 if TYPE_CHECKING:
     from bot import DuckBot
+    from logging import Logger
+
+T = TypeVar('T')
+P = ParamSpec('P')
+
+def add_logging(func: Callable[P, Union[Awaitable[T], T]]) -> Callable[P, Union[Awaitable[T], T]]:
+    """
+    Used to add logging to a coroutine or function.
+    
+    .. code-block:: python3
+        >>> async def foo(a: int, b: int) -> int:
+        >>>     return a + b
+        
+        >>> logger = add_logging(foo)
+        >>> result = await logger(1, 2)
+        >>> print(result)
+        3
+        
+        >>> def foo(a: int, b: int) -> int:
+        >>>     return a + b
+        
+        >>> logger = add_logging(foo)
+        >>> result = logger(1, 2)
+        >>> print(result)
+        3
+    """
+    async def _async_wrapped(*args: P.args, **kwargs: P.kwargs) -> Awaitable[T]:
+        start = time_lib.time()
+        result = await func(*args, **kwargs)  # type: ignore
+        print(f'{func.__name__} took {time_lib.time() - start:.2f} seconds')
+        
+        return result # type: ignore
+    
+    def _sync_wrapped(*args: P.args, **kwargs: P.kwargs) -> T:
+        start = time_lib.time()
+        result = func(*args, **kwargs)
+        print(f'{func.__name__} took {time_lib.time() - start:.2f} seconds')
+        
+        return result # type: ignore
+    
+    return _async_wrapped if asyncio.iscoroutinefunction(func) else _sync_wrapped # type: ignore
 
 
 class DuckCog(commands.Cog):
