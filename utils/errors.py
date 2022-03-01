aec8376e1c03503d9ecfefe2a4bc62dd69020b00
NextChai <75498301+NextChai@users.noreply.github.com>
2022-03-01 13:12:24 -0500

    Add HierarchyException

--- utils/errors.py
+++ utils/errors.py
@@ -10,10 +10,11 @@ import discord
 
 log = logging.getLogger('Duckbot.utils.errors')
 
-__all__: Tuple[str, ...] = (
-    'DuckBotException',
-    'DuckBotNotStarted',
-)
+# NOTE: Add me back later when all exceptions have been made
+#__all__: Tuple[str, ...] = (
+#    'DuckBotException',
+#    'DuckBotNotStarted',
+#)
 
 
 class DuckBotException(discord.ClientException):
@@ -26,13 +27,26 @@ class DuckBotNotStarted(DuckBotException):
     DuckBot is ready.
     """
     __slots__: Tuple[str, ...] = ()
+    
+    
+class HierarchyException(DuckBotException):
+    """Raised when DuckBot is requested to perform an operation on a member
+    that is higher than them in the guild hierarchy.
+    """
+    __slots__: Tuple[str, ...] = (
+        'member',
+    )
+    
+    def __init__(self, member: discord.Member) -> None:
+        self.member: discord.Member = member
+        super().__init__(f'{member}\'s top role, {member.top_role.mention} is higher than mine. I can not do this operation.')
 
 
 class TimerError(DuckBotException):
     """The base for all timer base exceptions. Every Timer based error should inherit
     from this.
     """
-
+    __slots__: Tuple[str, ...] = ()
 
 class TimerNotFound(TimerError):
     """Raised when trying to fetch a timer that does not exist."""
