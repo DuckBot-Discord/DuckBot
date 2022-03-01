298cf6e5c9a7b170caba1dfbdfa6930e0d3c0ef1
Leo <leonardocavenago05@gmail.com>
2022-03-01 01:16:22 +0100

    Fixing the prefixes.py some more

--- cogs/guild_config/prefixes.py
+++ cogs/guild_config/prefixes.py
@@ -13,19 +13,19 @@ from utils.time import human_join
 
 
 class PrefixChanges(DuckCog):
-    
+
     # NOTE: Delete me later
     @commands.command(hidden=True)
     @commands.is_owner()
     async def __give_error(self, ctx: DuckContext):
         raise discord.DiscordException('This is an error.')
-    
+
     @commands.group(name='prefix', aliases=['prefixes'], invoke_without_command=True)
     @commands.guild_only()
     async def prefix(self, ctx: DuckContext, *, prefix: Optional[str] = None) -> Optional[discord.Message]:
         """
         Adds a prefix for this server (you can have up to 25 prefixes).
-        
+
         Parameters
         ----------
         prefix: Optional[:class:`str`]
@@ -34,7 +34,7 @@ class PrefixChanges(DuckCog):
         """
         if ctx.invoked_subcommand:
             return
-        
+
         if prefix is None:
             prefixes = await self.bot.get_prefix(ctx.message, raw=True)
             embed = discord.Embed(title='Current Prefixes', description='\n'.join(prefixes))
@@ -45,10 +45,10 @@ class PrefixChanges(DuckCog):
         guild = ctx.guild
         if not guild:  # Safety net for type checker
             return
-        
+
         async with self.bot.safe_connection() as conn:
-            data = await conn.fetchrow('SELECT prefixes FROM guilds WHERE guild_id = $1', guild.id)
-        
+            data = await conn.fetchval('SELECT prefixes FROM guilds WHERE guild_id = $1', guild.id)
+
             if not data:
                 # This server had no existing prefix data but wants it, let's create it
                 query = 'INSERT INTO guilds(guild_id, prefixes) VALUES ($1, $2) RETURNING prefixes'
@@ -65,21 +65,19 @@ class PrefixChanges(DuckCog):
                 # This server had existing prefix data, append it onto the array of prefixes
                 query = 'UPDATE guilds SET prefixes = array_append(prefixes, $1) WHERE guild_id = $2 RETURNING prefixes'
                 args = (prefix, guild.id)
-                
-            result = await conn.fetchrow(query, *args)
-        
-        prefixes = result['prefixes']
-        
+
+            prefixes = await conn.fetchval(query, *args)
+
         # Cleanup bot cache before anything else
         self.bot.prefix_cache[guild.id] = set(prefixes)
-        
+
         embed = discord.Embed(
             title=f'Prefix "{prefix}" added.',
             description=f'I\'ve added the prefix `{prefix}` to this server.\n'
         )
         embed.add_field(name='Prefixes', value=human_join(prefixes, final='and'))
         return await ctx.send(embed=embed)
-    
+
     @prefix.command(name='clear', aliases=['wipe', 'whipe'])
     @commands.guild_only()
     async def prefix_clear(self, ctx: DuckContext) -> Optional[discord.Message]:
@@ -89,29 +87,29 @@ class PrefixChanges(DuckCog):
         guild = ctx.guild
         if guild is None:
             return
-        
+
         async with self.bot.safe_connection() as conn:
-            data = await conn.fetchval('SELECT prefixes FROM guilds WHERE guild_id = $1', guild.id)
-            
-            if not data:
+            old_prefixes = await conn.fetchval('SELECT prefixes FROM guilds WHERE guild_id = $1', guild.id)
+
+            if not old_prefixes:
                 # The bot doesn't have any prefixes for this server
                 embed = discord.Embed(title='No prefixes!')
                 embed.description = 'This server has no custom prefixes to clear! (only the default one)'
                 return await ctx.send(embed=embed)
-            
-            prefixes = await conn.fetchval('UPDATE guilds SET prefixes = $1 WHERE guild_id = $2 RETURNING prefixes', [], guild.id)
-        
+
+            await conn.fetchval('UPDATE guilds SET prefixes = $1 WHERE guild_id = $2', [], guild.id)
+
         # Cleanup bot cache before anything else
         self.bot.prefix_cache.pop(guild.id, None)
-        
+
         embed = discord.Embed(
-            title=f'Cleared {len(prefixes)} prefixes.',
+            title=f'Cleared {len(old_prefixes)} prefixes.',
             description=f'I\'ve cleared all prefixes from this server.\n'
         )
-        embed.add_field(name='Old Prefixes', value=human_join(prefixes, final='and') or '\u200b')
+        embed.add_field(name='Old Prefixes', value=human_join(old_prefixes, final='and') or '\u200b')
         return await ctx.send(embed=embed)
-      
-    @discord.utils.copy_doc(prefix)  
+
+    @discord.utils.copy_doc(prefix)
     @prefix.command(name='add', aliases=['append'])
     @commands.guild_only()
     async def prefix_add(self, ctx: DuckContext, *, prefix: str) -> Optional[discord.Message]:
@@ -122,7 +120,7 @@ class PrefixChanges(DuckCog):
     async def prefix_remove(self, ctx: DuckContext, *, prefix: str) -> Optional[discord.Message]:
         """
         Removes a prefix from the bots prefixes.
-        
+
         Parameters
         ----------
         prefix: :class:`str`
@@ -131,7 +129,7 @@ class PrefixChanges(DuckCog):
         guild = ctx.guild
         if not guild:
             return
-        
+
         prefixes = await self.bot.get_prefix(ctx.message, raw=True)
         if prefix not in prefixes:
             # NOTE: Add fuzzy matching here to suggest a prefix that might be the one they want
@@ -143,7 +141,7 @@ class PrefixChanges(DuckCog):
 
         async with self.bot.safe_connection() as conn:
             await conn.execute('UPDATE guilds SET prefixes = array_remove(prefixes, $1) WHERE guild_id = $2', prefix, guild.id)
-        
+
         # Cleanup cache before anything else
         try:
             self.bot.prefix_cache[guild.id].remove(prefix)
