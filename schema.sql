be0f9924f751d08c69423377999c966a69f7d54f
Leo <leonardocavenago05@gmail.com>
2022-03-01 13:47:40 +0100

    started working on the news

--- schema.sql
+++ schema.sql
@@ -2,3 +2,10 @@ CREATE TABLE IF NOT EXISTS guilds (
     guild_id BIGINT PRIMARY KEY,
     prefixes TEXT[] NOT NULL DEFAULT ARRAY[]::TEXT[]
 );
+
+CREATE TABLE IF NOT EXISTS news (
+    news_id BIGINT PRIMARY KEY,
+    title VARCHAR(256) NOT NULL,
+    content VARCHAR(1024) NOT NULL,
+    author_id BIGINT NOT NULL
+);
