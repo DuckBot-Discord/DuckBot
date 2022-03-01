158acf49bc63aac64e5f83b15b57b50a83f8900f
NextChai <75498301+NextChai@users.noreply.github.com>
2022-03-01 12:27:01 -0500

    Add timers sql

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS guilds (
     guild_id BIGINT PRIMARY KEY,
-    prefixes TEXT[] NOT NULL DEFAULT ARRAY[]::TEXT[]
+    prefixes TEXT[] NOT NULL DEFAULT ARRAY[]::TEXT[],
+    muted_role_id BIGINT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS news (
@@ -8,4 +9,13 @@ CREATE TABLE IF NOT EXISTS news (
     title VARCHAR(256) NOT NULL,
     content VARCHAR(1024) NOT NULL,
     author_id BIGINT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS timers(
+    id BIGSERIAL PRIMARY KEY,
+    percise BOOLEAN DEFAULT TRUE,
+    event TEXT,
+    extra JSONB,
+    created TIMESTAMP,
+    expires TIMESTAMP
 );
