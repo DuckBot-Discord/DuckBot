d37f7a5ee9ad5a3f7a3c754575fd2d84abb7b98f
NextChai <75498301+NextChai@users.noreply.github.com>
2022-04-04 13:14:57 -0400

    Update for debugging

--- cogs/owner/test_shit.py
+++ cogs/owner/test_shit.py
@@ -3,18 +3,24 @@ from typing import Optional
 
 from utils import DuckCog
 
-from utils.command import command
+from utils.command import command, group
 
 async def _constrict_input(ctx):
     return ('one', 'two')
 
 class TestingShit(DuckCog):
     
-    @command(name='cock')
+    @group(name='cock', invoke_without_command=True)
     async def cock(self, ctx, foo_bar: str = None, one: str = 'COCK') -> None: # type: ignore
+        if ctx.invoked_subcommand:
+            return
+        
         return await ctx.send(f'HII {one}')
     
     @cock.autocomplete('one')
     async def cock_autocomplete(self, ctx):
         return ('one', 'two')
     
+    @cock.command(name='foo')
+    async def cock_foo(self, ctx):
+        return await ctx.send('FOO')
