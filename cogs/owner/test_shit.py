380ba76180c8c1382b401cad52bd77c4a3ca42a7
NextChai <75498301+NextChai@users.noreply.github.com>
2022-04-03 20:49:39 -0400

    Update testing for autocomplete system

--- cogs/owner/test_shit.py
+++ cogs/owner/test_shit.py
@@ -1,7 +1,20 @@
 from __future__ import annotations
+from typing import Optional
 
 from utils import DuckCog
 
+from utils.command import command
+
+async def _constrict_input(ctx):
+    return ('one', 'two')
 
 class TestingShit(DuckCog):
-    ...  # For testing
+    
+    @command(name='cock')
+    async def cock(self, ctx, foo_bar: str = None, one: str = 'COCK') -> None: # type: ignore
+        return await ctx.send(f'HII {one}')
+    
+    @cock.autocomplete('one')
+    async def cock_autocomplete(self, ctx):
+        return ('one', 'two')
+    
