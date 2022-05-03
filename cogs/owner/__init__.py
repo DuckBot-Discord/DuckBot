fff79e92c44782704aadd055d9b2e47c70a1edc3
Leo <leonardocavenago05@gmail.com>
2022-05-04 00:33:44 +0200

    reloadall command

--- cogs/owner/__init__.py
+++ cogs/owner/__init__.py
@@ -1,5 +1,6 @@
-from utils import DuckContext, HandleHTTPException
-from discord.ext.commands import NotOwner, command
+from utils import DuckContext, HandleHTTPException, cb
+from discord.ext.commands import NotOwner, command, Paginator, ExtensionFailed
+from traceback import format_exception
 
 from .blacklist import BlackListManagement
 from .test_shit import TestingShit
@@ -31,6 +32,23 @@ class Owner(BlackListManagement, TestingShit,
         async with HandleHTTPException(ctx):
             await msg.edit(content=f'✅ Synced {len(cmds)} commands.')
 
+    @command()
+    async def rall(self, ctx):
+        paginator = Paginator(prefix='', suffix='')
+        for extension in list(self.bot.extensions.keys()):
+            try:
+                await self.bot.reload_extension(extension)
+                paginator.add_line(f"\N{CLOCKWISE RIGHTWARDS AND LEFTWARDS OPEN CIRCLE ARROWS} `{extension}`")
+
+            except Exception as e:
+                if isinstance(e, ExtensionFailed):
+                    e = e.original
+                paginator.add_line(f'\N{WARNING SIGN} Failed to load extension: `{extension}`', empty=True)
+                error = ''.join(format_exception(type(e), e, e.__traceback__))
+                paginator.add_line(cb(error))
+
+        for page in paginator.pages:
+            await ctx.send(page)
 
 async def setup(bot):
     await bot.add_cog(Owner(bot))
